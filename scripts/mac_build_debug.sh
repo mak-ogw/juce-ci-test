5f1cd95e983d34edf14431145246866e68a9a7ba
mak-ogw <makotogawa.corp@gmail.com>
2024-07-15 10:47:20 +0900

    initial commit

---
+++ scripts/mac_build_debug.sh
@@ -0,0 +1,3 @@
+rm -rf build_xcode
+cmake . -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" -G "Xcode" -DJUCE_BUILD_EXAMPLES=OFF -Bbuild_xcode
+xcodebuild -project build_xcode/juce-ci-test.xcodeproj -configuration Debug -target ALL_BUILD
