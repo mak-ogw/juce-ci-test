17089c5ce4591e37fc31f51550f3553b9a931c9c
mak-ogw <makotogawa.corp@gmail.com>
2024-07-15 10:57:03 +0900

    update mac_build_release.sh

--- scripts/mac_build_release.sh
+++ scripts/mac_build_release.sh
@@ -1,3 +1,4 @@
+git submodule update --remote --recursive
 rm -rf build_xcode
 cmake . -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" -G "Xcode" -DJUCE_BUILD_EXAMPLES=OFF -Bbuild_xcode
 xcodebuild -project build_xcode/juce-ci-test.xcodeproj -configuration Release -target ALL_BUILD
